fb35984e78810cf58b8b3def8d4ca133e5830534
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2024-09-17 15:37:12 -0300

    up

--- frontend/src/components/aluno/EditarAluno.jsx
+++ frontend/src/components/aluno/EditarAluno.jsx
@@ -1,44 +1,45 @@
-import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const url = "http://localhost:3001/";
+const url = 'http://localhost:3001/';
 
 const endpoints = {
-  alunos: `${url}alunos/`
+  alunos: `${url}alunos/`,
 };
 
-
 const EditarAluno = () => {
-  const [nome, setNome] = useState("");
-  const [curso, setCurso] = useState("RC");
-  const [ira, setIra] = useState("");
-  
+  const [nome, setNome] = useState('');
+  const [curso, setCurso] = useState('RC');
+  const [ira, setIra] = useState('');
+
   const { id } = useParams(); // Pegando o ID do aluno pela URL
   const navigate = useNavigate(); // Para redirecionar após edição
 
   useEffect(() => {
-    
-    axios.get(`${endpoints.alunos}${id}`)
-      .then((response) => {
+    axios
+      .get(`${endpoints.alunos}${id}`)
+      .then(response => {
         const { nome, curso, ira } = response.data;
+        console.log(response.data);
         setNome(nome);
         setCurso(curso);
         setIra(ira);
       })
-      .catch((error) => console.log(error));
+      .catch(error => console.log(error));
   }, [id]);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
     const alunoEditado = { nome, curso, ira };
 
-    axios.put(`${endpoints.alunos}atualizar/${id}`, alunoEditado)
-      .then((response) => {
+    axios
+      .put(`${endpoints.alunos}editar/${id}`, alunoEditado)
+      .then(response => {
         console.log(response);
-        navigate("/alunos/listar");
+        navigate('/alunos/listar');
       })
-      .catch((error) => console.log(error));
+      .catch(error => console.log(error));
   };
 
   return (
@@ -46,47 +47,67 @@ const EditarAluno = () => {
       <h1>Editar Aluno</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="idNome" className="form-label">Nome</label>
+          <label
+            htmlFor="idNome"
+            className="form-label"
+          >
+            {nome}
+          </label>
           <input
             id="idNome"
             type="text"
             name="nome"
             value={nome}
-            onChange={(event) => setNome(event.target.value)}
+            onChange={event => setNome(event.target.value)}
             className="form-control"
           />
         </div>
 
         <div>
-          <label htmlFor="idCurso" className="form-label">Curso</label>
+          <label
+            htmlFor="idCurso"
+            className="form-label"
+          >
+            {curso}
+          </label>
           <select
             id="idCurso"
             value={curso}
-            onChange={(event) => setCurso(event.target.value)}
+            onChange={event => setCurso(event.target.value)}
             className="form-select"
           >
-            <option value="RC">Redes</option>
-            <option value="CC">Ciência</option>
+            <option value="RC">Redes de Computadores</option>
+            <option value="CC">Ciência da Computação</option>
             <option value="ES">Engenharia de Software</option>
-            <option value="DD">Design</option>
+            <option value="DD">Design Digital</option>
             <option value="EC">Engenharia da Computação</option>
           </select>
         </div>
 
         <div>
-          <label htmlFor="idIra" className="form-label">IRA</label>
+          <label
+            htmlFor="idIra"
+            className="form-label"
+          >
+            {ira}
+          </label>
           <input
             id="idIra"
             type="text"
             name="ira"
             value={ira}
-            onChange={(event) => setIra(event.target.value)}
+            onChange={event => setIra(event.target.value)}
             className="form-control"
           />
         </div>
 
         <div className="div-button-submit">
-          <button type="submit" className="btn btn-primary">ATUALIZAR</button>
+          <button
+            type="submit"
+            className="btn btn-primary"
+          >
+            ATUALIZAR
+          </button>
         </div>
       </form>
     </div>
